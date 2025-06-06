d983e1fd0acb4472ec261b8caf84e8411c0b5867
Husnain Ali <128280622+husnainalyy@users.noreply.github.com>
2025-06-06 05:02:31 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
 # check if port variable is set or go with default
-if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT="$PORT"; echo "N8N will start on '$PORT'"; fi
+if [ -z ${PORT+x} ]; then 
+  echo "PORT variable not defined, leaving N8N to default port."
+else 
+  export N8N_PORT="$PORT"
+  echo "N8N will start on '$PORT'"
+fi
 
 # regex function
 parse_url() {
   eval $(echo "$1" | sed -e "s#^\(\(.*\)://\)\?\(\([^:@]*\)\(:\(.*\)\)\?@\)\?\([^/?]*\)\(/\(.*\)\)\?#${PREFIX:-URL_}SCHEME='\2' ${PREFIX:-URL_}USER='\4' ${PREFIX:-URL_}PASSWORD='\6' ${PREFIX:-URL_}HOSTPORT='\7' ${PREFIX:-URL_}DATABASE='\9'#")
 }
 
-# prefix variables to avoid conflicts and run parse url function on arg url
+# parse Heroku DATABASE_URL
 PREFIX="N8N_DB_" parse_url "$DATABASE_URL"
 echo "$N8N_DB_SCHEME://$N8N_DB_USER:$N8N_DB_PASSWORD@$N8N_DB_HOSTPORT/$N8N_DB_DATABASE"
-# Separate host and port    
+
+# Extract host/port
 N8N_DB_HOST="$(echo $N8N_DB_HOSTPORT | sed -e 's,:.*,,g')"
 N8N_DB_PORT="$(echo $N8N_DB_HOSTPORT | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
 
@@ -22,5 +28,8 @@ export DB_POSTGRESDB_DATABASE=$N8N_DB_DATABASE
 export DB_POSTGRESDB_USER=$N8N_DB_USER
 export DB_POSTGRESDB_PASSWORD=$N8N_DB_PASSWORD
 
-# kickstart nodemation
+# 🔐 SSL REQUIRED FOR HEROKU POSTGRESQL
+export DB_POSTGRESDB_SSL_REJECT_UNAUTHORIZED=false
+
+# kickstart n8n
 n8n
